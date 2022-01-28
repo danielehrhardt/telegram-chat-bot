a6d0d0e710a407344dd651ec6b16f95c714ee7f4
Daniel Ehrhardt <daniel.ehrhardt@codext.de>
2022-01-28 20:25:48 +0100

    Added Home

--- angular/src/app/home/home.page.ts
+++ angular/src/app/home/home.page.ts
@@ -12,6 +12,7 @@ const api_hash = '6f541bce03b3699c9eade214311b475b';
 })
 export class HomePage implements OnInit {
   api;
+  user;
   country: string;
   constructor(private loadingCtrl: LoadingController) {}
 
@@ -28,5 +29,78 @@ export class HomePage implements OnInit {
     this.country = result.country;
 
     loading.dismiss();
+    await this.initUser();
+  }
+
+  async sendMessageToUserId(userId: string, message: string) {
+    await this.api.call('messages.sendMessage', {
+      clear_draft: true,
+      peer: {
+        _: 'inputPeerUser',
+        user_id: userId,
+      },
+      message: message,
+
+      random_id:
+        Math.ceil(Math.random() * 0xffffff) +
+        Math.ceil(Math.random() * 0xffffff),
+    });
+  }
+
+  async initUser() {
+    const user = await this.getUser();
+    const phone = '+4915161018772';
+    if (!user) {
+      const { phone_code_hash } = await this.sendCode(phone);
+      const code = await prompt('Enter your Code: ');
+
+      try {
+        const signInResult = await this.signIn({
+          code,
+          phone,
+          phone_code_hash,
+        });
+      } catch (error) {
+        if (error.error_message !== 'SESSION_PASSWORD_NEEDED') {
+          console.log(`error:`, error);
+
+          return;
+        }
+      }
+    } else {
+      this.user = user;
+      console.log('user:', user);
+    }
+  }
+
+  async getUser() {
+    try {
+      const user = await this.api.call('users.getFullUser', {
+        id: {
+          _: 'inputUserSelf',
+        },
+      });
+
+      return user;
+    } catch (error) {
+      return null;
+    }
+  }
+
+  sendCode(phone) {
+    return this.api.call('auth.sendCode', {
+      phone_number: phone,
+      settings: {
+        _: 'codeSettings',
+      },
+    });
+  }
+
+  signIn({ code, phone, phone_code_hash }) {
+    return this.api.call('auth.signIn', {
+      phone_code: code,
+      phone_number: phone,
+      phone_code_hash: phone_code_hash,
+    });
   }
 }
