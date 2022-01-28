e989c652c84e7d0031e3c51ca0f453ebc4d87c74
Daniel Ehrhardt <daniel.ehrhardt@codext.de>
2022-01-29 00:35:41 +0100

    U

--- angular/src/app/home/home.page.ts
+++ angular/src/app/home/home.page.ts
@@ -50,30 +50,38 @@ export class HomePage implements OnInit {
       });
 
       if (fullChat) {
-        await fullChat.users.map(async (_) => {
-          await this.api.call('messages.sendMessage', {
-            clear_draft: true,
-            peer: {
-              _: 'inputPeerUser',
-              user_id: _.id,
-            },
-            message: message,
-            entities: [
-              {
-                _: 'messageEntityBold',
-                offset: 6,
-                length: 13,
+        await Promise.all(
+          fullChat.users.map(async (_) => {
+            console.log('sendMessage -> ', _, message);
+            await this.api.call('messages.sendMessage', {
+              clear_draft: true,
+              peer: {
+                _: 'inputPeerUser',
+                user_id: _.id,
               },
-            ],
+              message: message,
+              entities: [
+                {
+                  _: 'messageEntityBold',
+                  offset: 6,
+                  length: 13,
+                },
+              ],
 
-            random_id:
-              Math.ceil(Math.random() * 0xffffff) +
-              Math.ceil(Math.random() * 0xffffff),
-          });
-          return _;
-        });
+              random_id:
+                Math.ceil(Math.random() * 0xffffff) +
+                Math.ceil(Math.random() * 0xffffff),
+            });
+            await this.sleep((Math.floor(Math.random() * 10) + 1) * 100);
+            return _;
+          })
+        );
       }
     }
+  }
+
+  async sleep(milliseconds) {
+    return new Promise((resolve) => setTimeout(resolve, milliseconds));
   }
 
   async sendMessageToUserId(userId: string, message: string) {
@@ -93,10 +101,12 @@ export class HomePage implements OnInit {
 
   async initUser() {
     const user = await this.getUser();
-    const phone = '+4915161018772';
+
+    // const phone = '+4915161018772';
     if (!user) {
+      const phone = await prompt('Gebe deine Handynummer ein: ');
       const { phone_code_hash } = await this.sendCode(phone);
-      const code = await prompt('Enter your Code: ');
+      const code = await prompt('Gebe den Code von Telegram ein: ');
 
       try {
         const signInResult = await this.signIn({
